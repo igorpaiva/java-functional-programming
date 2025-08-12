07caf11b91f8977186313e512b7a9da1e5f803bc
Igor Paiva <igor.araujo.ee@gmail.com>
2025-08-12 13:53:22 -0300

    Add calculator example and simple lambda implementation

--- lambdas-and-streams/src/main/java/org/example/Main.java
+++ lambdas-and-streams/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         Greeting greeting = new HelloGreeting();
@@ -12,5 +14,13 @@ public class Main {
             }
         };
         greeting2.sayHello();
+
+        Calculator calculator = (int a, int b) -> {
+            Random rand = new Random();
+            int randomNumber = rand.nextInt(50);
+            return a * b + randomNumber;
+        };
+
+        System.out.println(calculator.calculate(5, 6));
     }
 }
